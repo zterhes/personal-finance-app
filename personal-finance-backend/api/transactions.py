ef3836960486c6f5988d584960ab60798c43f618
zterhes <zterhes1@gmail.com>
2024-10-09 01:15:59 +0200

    implementation

--- personal-finance-backend/api/transactions.py
+++ personal-finance-backend/api/transactions.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restx import Resource
 from server.instance import server
 from service.transaction_service import TransactionService
+from lib.auth.AuthProvider import authProvider
+
 
 transaction_service = TransactionService()
 
@@ -9,7 +11,13 @@ app = server.app
 api = server.api
 
 
+@app.before_request
+def before_request():
+    request.clerk = authProvider.authenticate_session()
+
+
 @api.route("/transactions")
 class Transactions(Resource):
     def get(self):
+        session = request.clerk
         return transaction_service.get_transactions()
--- personal-finance-backend/environment/configuration.py
+++ personal-finance-backend/environment/configuration.py
@@ -1,11 +1,25 @@
 import os
 
-env = os.environ.get("ENV_PROFILE")
+env = os.environ.get("ENV_PROFILE", "development")
 port = os.environ.get("PORT", 8080)
+api_key = os.environ.get("CLERK_API_KEY")
+
+if api_key is None:
+    raise Exception("CLERK_API_KEY must be set")
 
 all_environments = {
-    "development": {"port": port, "debug": True, "swagger-url": "/api/swagger"},
-    "production": {"port": port, "debug": False, "swagger-url": None},
+    "development": {
+        "port": port,
+        "debug": True,
+        "swagger-url": "/api/swagger",
+        "api-key": api_key,
+    },
+    "production": {
+        "port": port,
+        "debug": False,
+        "swagger-url": None,
+        "api-key": api_key,
+    },
 }
 print(all_environments[env])
 environment_config = all_environments[env]
